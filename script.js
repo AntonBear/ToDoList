e8b64b1aed29391ad534a5351503a7c48cc78da0
AntonBear <holzed15@gmail.com>
2024-06-09 01:08:49 +0300

    work

--- script.js
+++ script.js
@@ -2,16 +2,44 @@
 
 // parentElement.appendChild(newDiv)
 
-const btn1 = document.querySelector('.btn1')
+let parentElement = document.querySelector('.collector')
+let addTask = document.querySelector('.addTask')
+let addTask1 = document.querySelector('.addTask1')
+let ol = document.querySelector('.ol')
+let taskLi = document.querySelector('.taskLi')
+let taskDiv = document.querySelector('.taskDiv')
+let buttonDeleteTask = document.querySelector('.buttonDeleteTask')
+let buttonAccept = document.querySelector('.buttonAccept')
+let inputText = document.querySelector('.inputText')
+// let li = document.querySelector('.li')
+let addTaskSUV = document.querySelector('.addTaskSUV')
 
-btn1.addEventListener('click', function addDiv() {
-  const parentElement = document.querySelector('.collector')
-  const task = document.querySelector('.task')
-  const newTask = task.cloneNode(true)
-  // const textNewDiv = document.createTextNode('Hello')
-  // newDiv.appendChild(textNewDiv)
-  parentElement.appendChild(newTask)
+addTask.addEventListener('click', function () {
+  let newTask = taskDiv.cloneNode(true)
+  ol.append(newTask)
 })
+
+addTask.addEventListener('click', function () {
+  // taskLi.classList.forEach((item) => {      // work
+  //   console.log(item)
+  // })
+
+  let elements = document.querySelectorAll('li') // work
+  console.log(elements.length)
+
+  for (let i = 0; i < elements.length; i++) {
+    elements.forEach((item) => {
+      item.className = `TaskLi-${i}`
+    })
+  }
+})
+
+buttonAccept.onclick = function () {
+  let inputValue = inputText.value
+  console.log(inputValue)
+}
+
+buttonDeleteTask.addEventListener('click', function () {})
 
 // let div = document.createElement('div')
 // div.className = 'task'
@@ -26,11 +54,6 @@ btn1.addEventListener('click', function (e) {
   document.querySelector('.counter').innerHTML = i
 })
 
-// document.addEventListener('click', e => {
-//   var btn = e.target.closest('button')
-//   if (btn) ++btn.textContent
-// })
-
 class ToDoList {
   constructor() {
     this.posts = 0
@@ -40,40 +63,3 @@ class ToDoList {
 
 const arr = []
 const counter = function () {}
-
-// const btn1 = document.querySelector('.btn1')
-
-// bnt1.addEventListener('click', div)
-
-// function go() {
-//   console.log('Polina')
-// }
-
-// let div = document.createElement('div')
-// div.innerHTML = '<strong>Всем привет!</strong> Вы прочитали важное сообщение.'
-
-// document.body.append(div)
-
-/*
-
-<div id="row">
-<div class="page-block">
-Страница 1
-</div>
-</div> 
-<button class="addBtn">+Страница</button>
-<button class="addBtn">Готово</button>
-
-
-const addBtn = document.querySelector('.addBtn');
-var blockNumber = 2;
-addBtn.onclick = () => {
-  // Создаем блок и добавляем номер
-  const createBlock = document.createElement('div');
-  createBlock.className = 'page-block';
-  createBlock.innerText = 'Страница ' + blockNumber;
-  blockNumber++;
-  addBtn.insertAdjacentElement('beforebegin', createBlock);
-}
-
-*/
