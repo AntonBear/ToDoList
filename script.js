40dfb59d3b21b565195df9d60da3c16225dc5a58
AntonBear <holzed15@gmail.com>
2024-06-09 12:54:58 +0300

    11

--- script.js
+++ script.js
@@ -14,24 +14,52 @@ let inputText = document.querySelector('.inputText')
 // let li = document.querySelector('.li')
 let addTaskSUV = document.querySelector('.addTaskSUV')
 
-addTask.addEventListener('click', function () {
-  let newTask = taskDiv.cloneNode(true)
-  ol.append(newTask)
-})
+// addTask.addEventListener('click', function () {
+//   let newTask = taskDiv.cloneNode(true)
+//   ol.append(newTask)
+// })
 
 addTask.addEventListener('click', function () {
-  // taskLi.classList.forEach((item) => {      // work
+  // пробую сделать добавление нового задания правильно
+  let newTask = taskLi.cloneNode(true)
+  ol.append(newTask)
+
+  // работающий переборщик названий классов
+  // taskLi.classList.forEach((item) => {
+  //   // work
   //   console.log(item)
   // })
 
-  let elements = document.querySelectorAll('li') // work
-  console.log(elements.length)
+  // рабочий вывод длинны списка
+  // let elements = document.querySelectorAll('li') // work
+  // console.log(elements.length)
 
-  for (let i = 0; i < elements.length; i++) {
-    elements.forEach((item) => {
-      item.className = `TaskLi-${i}`
-    })
-  }
+  // работающий заменитель классов
+  // let elements = document.querySelectorAll('li') // work
+  // for (let i = 1; i < elements.length; i++) {
+  //   elements.forEach((item) => {
+  //     item.className = `TaskLi-${i + 1}`
+  //   })
+  // }
+
+  // НЕ СРАБОТАЛ, но потом заработал ))
+  let elements = document.querySelectorAll('li')
+  let buttom = document.querySelectorAll('div > button')
+
+  let counter = 0
+  let counter1 = 0
+  elements.forEach((item) => {
+    counter++
+    item.classList = `TaskLi-${counter}`
+  })
+
+  buttom.forEach((item) => {
+    counter1++
+    item.classList = `buttonDelete-${counter1}`
+  })
+
+  // попробуй воспольоваться методом
+  // const spanList = [...document.querySelectorAll("span")];
 })
 
 buttonAccept.onclick = function () {
